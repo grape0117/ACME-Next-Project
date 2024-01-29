8aab78db83d94d0944d505b3e1be682413fa6671
kysm1226 <janesan20000206@gmail.com>
2024-01-29 21:07:16 +0100

    initial

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Page(){
+    return(
+        <p>Here is Customers Page.</p>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import SideNav from "@/app/ui/dashboard/sidenav"
+
+export default function Page(){
+    return(
+        <p>Here is Dashboard</p>
+    )
+}
